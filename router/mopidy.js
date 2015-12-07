63c1697ca0be3ff01248892ac35b4c751d141d40
Jason Gao <jj689x@gmail.com>
2015-12-07 00:06:49 -0500

    get mopidy status

--- router/mopidy.js
+++ router/mopidy.js
@@ -6,6 +6,15 @@ var express = require('express'),
 	framework = require('../framework/app');
 
 router
+	.get('/', function(req,res) {
+		mopidy.execAction('mopidy','getStatus')
+			.then(function(stdout) {
+				res.json(stdout);
+			},
+			function(err) {
+				res.json({ err: err });
+			});
+	})
 	.get('/:action', function(req,res) {
 		var response = mopidy.execAction('mopidy', 'set' + framework.uppercaseFirst(req.params.action));
 		response.then(function(stdout) {
