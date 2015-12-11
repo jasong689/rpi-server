ee0dc8be9b8f8a754a59a899e7e158c8765a7730
Jason Gao <jj689x@gmail.com>
2015-12-11 02:22:56 -0500

    add some methods to singleton

--- mods/mod-mopidy.js
+++ mods/mod-mopidy.js
@@ -11,14 +11,57 @@ var System = require('../framework/mod'),
 // messages to mpd server
 var mpd = (function() {
 	var _instance = null,
-		_port = '6680';
+		_port = '6680',
+		// stored here for now
+		// move to db
+		_password = '',
+		_ready = false,
+		_commands = [],
+		_public = {};
 
 	function getInstance() {
 		if (_instance === null) {
-			_instance = new net.Socket();
-			_instance.connect({port:_port});
+			// checks our array for any new/pending messages
+			var write = function() {
+				var first = _commands.first();
+				if (first.status === 'pending') {
+					return false;
+				} else if (first.status == 'new') {
+					inst.write(message);
+					first.status = 'pending';
+				}
+			};
+
+			_instance = net.createConnection({port:_port});
+			_instance.setEncoding(encoding);
+			_instance.write('password ' + _password + '\n');
+			// once a response is received we can resolve its
+			// promise with the data and proceed with the next message
+			_instance.once('data', function(data) {
+				_ready = true;
+				_instance.on('data', function(data) {
+					var command = commands.shift();
+					command.promise.resolve(data);
+					write();
+				});
+			});
+
+			// add a new message to commands
+			// and see if we can write it
+			var send = function(message,p) {
+				if  (!_ready) return false;
+				var command = { 
+					command: message,
+					status: 'new',
+					promise: p
+				};
+				_commands.push(command);
+				write()
+			};
+
+			_public.send = send;
 		}
-		return _instance;
+		return _public;
 	}
 	
 	return {
@@ -30,9 +73,7 @@ sys.getPlayerstatus = function getPlayerstatus() {
 	var def = Promise.deferred(),
 		so = mpd.getInstance();
 
-	so.write('status',encoding,function(data) {
-		def.resolve({ resp: data });
-	});
+	so.send('status\n',def);
 
 	return def;
 };
@@ -63,6 +104,10 @@ sys.setStop = function setStop() {
 	return this.exec('service mopidy stop').then(function(stdout) {
 		return { resp: stdout };
 	});
+}
+
+function buildCommand(command) {
+	return 'command_list_begin\n' + command + 'command_list_end\n';
 }
 
 module.exports = sys;
